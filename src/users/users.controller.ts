82618fb9ca631f6cb4187e7972cd12675a31d398
maaaz5 <mohamade.amine12345@gmail.com>
2024-02-26 00:29:37 +0100

    serialization of user entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,12 @@ import {
   Body,
   Delete,
   NotFoundException,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -22,6 +24,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(SerializeInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
